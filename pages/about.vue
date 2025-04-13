b2217ff5e442f83f3b2068ab92f9e4dcb48ae515
John Rocha <johnerickrocha@gmail.com>
2025-04-13 03:11:07 -0300

    feature. criar página About

---
+++ pages/about.vue
@@ -0,0 +1,189 @@
+<template>
+  <div class="min-h-screen bg-black text-white">
+    <!-- Imagem central no topo -->
+    <section class="flex justify-center py-12">
+      <img
+        src="../assets/images/path2.png"
+        alt="Portal Alquímico"
+        class="h-auto w-auto rounded-lg shadow-lg hover:scale-105 transition-transform duration-500"
+      />
+    </section>
+
+    <!-- Seções intercaladas -->
+    <div class="container mx-auto px-4 space-y-16 py-8">
+      <section class="flex flex-col md:flex-row items-center gap-8">
+        <div class="md:w-1/2">
+          <img
+            src="../assets/images/weapons.png"
+            alt="Introdução"
+            class="w-full rounded-lg shadow-lg hover:scale-105 transition-transform duration-500"
+          />
+        </div>
+        <div class="md:w-1/2 antiga">
+          <h2 class="text-3xl font-bold mb-4 text-yellow-400">
+            Jornada
+          </h2>
+          <p class="text-lg">
+            Você acaba de atravessar as portas do conhecimento oculto. Aqui,
+            alquimistas, magos e buscadores do saber ancestral encontram
+            orientação, inspiração e ferramentas para desvendar os mistérios da
+            transformação pessoal e espiritual.
+          </p>
+        </div>
+      </section>
+
+      <section class="flex flex-col md:flex-row-reverse items-center gap-8">
+        <div class="md:w-1/2">
+          <img
+            src="../assets/images/alchemical-circle.png"
+            alt="Fundador"
+            class="w-full rounded-lg shadow-lg hover:scale-105 transition-transform duration-500"
+          />
+        </div>
+        <div class="md:w-1/2 antiga">
+          <h2 class="text-3xl font-bold mb-4 text-yellow-400">
+            Alquimista Luria
+          </h2>
+          <p class="text-lg antiga">
+            Há quem diga que a magia é para selecionados, para mim foi mostrado:
+            A Magia é aquele pequeno movimento, o simples gesto do seu olhar
+            acompanhando as letras. Sua respiração fluindo naturalmente, o
+            tremular do gesto do sorriso. O parar, o desacelerar. A sua presença
+            é uma fonte de luz, uma fonte de sabedoria. Ser. Sentir da sua
+            presença.Sentir a presença do ambiente atravessando todo seu Ser. A
+            Magia é entender as forças que regem estes mecânismos. E criar.
+          </p>
+        </div>
+      </section>
+
+      <section class="flex flex-col md:flex-row items-center gap-8">
+        <div class="md:w-1/2">
+          <img
+            src="../assets/images/mission.png"
+            alt="Missão"
+            class="w-full rounded-lg shadow-lg hover:scale-105 transition-transform duration-500"
+          />
+        </div>
+        <div class="md:w-1/2 antiga">
+          <h2 class="text-3xl font-bold mb-4 text-yellow-400">Objetivo</h2>
+          <div class="text-lg mb-4">
+            Tudo está aqui. Para ser feito. Tudo está almejando existir. Dentro
+            de nós pulsam as forças que regem este mundo.
+
+            Para algo existir e ser louvávelmente imprescindível assim como é 
+            necessário a água para saciar a sede ela deve haver:
+            <br><br>
+            <div class="antiga-bold">Equilíbrio:</div> 
+            Condição de ajuste em que diferentes forças ou elementos se compensam.
+            <br><br>
+            <p class="antiga-bold">Harmonia:</p>
+            Combinação agradável, coerente e integrada de diferentes elementos.
+          </div>
+        </div>
+      </section>
+
+      <section class="flex flex-col items-center justify-center py-12 text-center">
+        <div class="max-w-3xl px-4 antiga">
+          <h2 class="text-3xl font-bold mb-8 text-yellow-400">
+            Sagrada artes ocultas
+          </h2>
+          <ul class="pl-5 space-y-2 text-lg inline-block">
+            <li>Alquimia (teórica e prática)</li>
+            <li>Magia cerimonial</li>
+            <li>Hermetismo</li>
+            <li>Cabala</li>
+            <li>Tarot</li>
+          </ul>
+        </div>
+        <img
+          src="../assets/images/knowledge.png"
+          alt="Portal Alquímico"
+          class="h-auto w-auto rounded-lg shadow-lg hover:scale-105 transition-transform duration-500 pt-7"
+        />
+      </section>
+
+      <section class="flex flex-col md:flex-row-reverse items-center gap-8">
+        <div class="md:w-1/2">
+          <img
+            src="../assets/images/logos.png"
+            alt="Metodologia"
+            class="w-full rounded-lg shadow-lg hover:scale-105 transition-transform duration-500"
+          />
+        </div>
+        <div class="md:w-1/2 antiga">
+          <h2 class="text-3xl font-bold mb-4 text-yellow-400">
+             Metodologia
+          </h2>
+          <ul class="list-disc pl-5 space-y-2 text-lg">
+            <li>Acesso pessoal</li>
+            <li>Biblioteca</li>
+            <li>Rituais coletivos</li>
+            <li>Atendimento exclusivo</li>
+          </ul>
+        </div>
+      </section>
+
+      <section class="flex flex-col md:flex-row items-center gap-8">
+        <div class="md:w-1/2">
+          <img
+            src="../assets/images/public.png"
+            alt="Público"
+            class="w-full rounded-lg shadow-lg hover:scale-105 transition-transform duration-500"
+          />
+        </div>
+        <div class="md:w-1/2 antiga">
+          <h2 class="text-3xl font-bold mb-4 text-yellow-400">
+            Estes são para
+          </h2>
+          <ul class="list-disc pl-5 space-y-2 text-lg">
+            <li>Pessoas interessadas em hermetismo, simbolismo e ocultismo</li>
+            <li>Estudantes iniciantes e avançados de magia e alquimia</li>
+            <li>Quem sente o chamado para o caminho da magia</li>
+          </ul>
+        </div>
+      </section>
+
+      <section class="flex flex-col md:flex-row-reverse items-center gap-8">
+        <div class="md:w-1/2">
+          <img
+            src="../assets/images/ritual.png"
+            alt="Privacidade"
+            class="w-full rounded-lg shadow-lg hover:scale-105 transition-transform duration-500"
+          />
+        </div>
+        <div class="md:w-1/2 antiga">
+          <h2 class="text-3xl font-bold mb-4 text-yellow-400">
+            Disciplina e consistência
+          </h2>
+          <p class="text-lg">
+            Compromisso espiritual e ético inabalável com conteúdo, estudos e
+            segurança dos conhecimentos aprofundados, especialmente relevante em
+            temas sensíveis como magia e ocultismo.
+          </p>
+        </div>
+      </section>
+
+      <section class="flex flex-col md:flex-row items-center gap-8 pb-16">
+        <div class="md:w-1/2">
+          <img
+            src="../assets/images/invoke.png"
+            alt="Contato"
+            class="w-full rounded-lg shadow-lg hover:scale-105 transition-transform duration-500"
+          />
+        </div>
+        <div class="md:w-1/2 antiga">
+          <h2 class="text-3xl font-bold mb-4 text-yellow-400">Me invoque</h2>
+          <p class="text-lg mb-4">
+            Me contate para agendar leitura e afins.
+          </p>
+          <a
+            href="/contact"
+            class="inline-block px-6 py-3 bg-yellow-400 text-black font-bold rounded hover:bg-yellow-300 transition-colors duration-300"
+          >
+            Enviar Mensagem
+          </a>
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
