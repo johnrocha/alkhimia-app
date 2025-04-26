9211a01071b9a5f5abcdb7fa57fc863b220ed9da
John Rocha <johnerickrocha@gmail.com>
2025-04-25 21:34:52 -0300

    feat: add rubedo page and update index page with new contentl

---
+++ pages/rubedo.vue
@@ -0,0 +1,72 @@
+<template>
+  <main class="container mx-auto px-4 space-y-16 py-8">
+    <section class="flex justify-center py-9">
+      <img
+        src="../assets/images/rubedo-head.png"
+        alt="Círculo Alquímico"
+        class="w-auto h-auto hover:scale-105 transition-transform duration-500 ease-in-out"
+      />
+    </section>
+    <section
+      class="flex flex-col items-center justify-center py-12 text-center"
+    >
+      <div class="max-w-3xl px-4 antiga">
+        <h2 class="text-5xl font-bold mb-8 text-yellow-400">Rubedo</h2>
+      </div>
+    </section>
+
+    <section class="flex flex-col md:flex-row items-center gap-8">
+      <div class="md:w-1/2">
+        <img
+          src="../assets/images/rubedo.png"
+          alt="Introdução"
+          class="w-full rounded-lg shadow-lg hover:scale-105 transition-transform duration-500"
+        />
+      </div>
+      <div class="md:w-1/2 antiga">
+        <h2 class="text-3xl font-bold mb-4 text-yellow-400">Princípio 4</h2>
+        <p class="text-lg">
+          O estágio dos grandes milagres, o cálice anseia receber a amálgama da
+          fênix. Eu morri tantas vezes, mas desta vez mais estarei tal como a
+          gruta de animais unicos, água ultra cristalina e pura, flores de azul
+          tão profundo que faria até mesmo um cego chorar em sua presença, um
+          lugar sagrado. Desta vez eu conseguiria ser morada de Deus? Afago
+          pois, as grandes respostas raramente vem quando as perseguimos. É
+          preciso estar atento. Eu me esvaziei, minha jornada até aqui foi um
+          grande, desaprender. Sei. Não basta querer, a tintura não é sobre cor,
+          é sobre se tornar... Auri pigmentum! Eu me vi reflexo, segui a luz e
+          então me torno o Sol. Coniunctio. Sim eu era reflexo, a lua, e agora
+          sou sol a luz progenitora. Ó silêncio mãe de todas as misteriosas
+          cousas. Augoeides! Que belo casamento! Enfim posso ser livre.
+          Excelsior! O milagre de uma única coisa. Domei as bestas que haviam em
+          mim. Belial! Leviatã! Lúcifer! Satã! O leão devora a serpente. O
+          mercúrio me gera infinitas flores. Estou com fome e sua música me
+          inunda, as seis crisálidas próximas de nascer, elas devoraram meu
+          alimento, mas meu alimento tem raíz. Ambos morreram e ambos estão
+          prestes a renascer. A matriarca desconhecida se foi, ela cuidou de mim
+          com tanto amor sem ao menos eu saber a primeira letra de seu nome. Ela
+          foi morta pela extrema ganância de seus próprios filhos. Ó chumbo. Eu
+          te irradio. Incandescente, eu ainda sou este peso, eu sou o pecado, eu
+          sou estas lágrimas, minhas mãos soam, estou morrendo. Fogo celeste me
+          leve para esta experência última. Bastet me pede carinho pois sou sua
+          mãe. No labirinto que é a minha mente. Encontrei um baú de tesouro,
+          dentro o fio de prata, eu segui o rolo e agora estou de joelhos a
+          Asherat, a rainha dos céus. Ó Nuit, por que lhe abandonamos? Agora eu
+          retorno, humilde. Envergonhado e com temperança. Enfim, nem que seja
+          por um momento tão sublime, tão efêmero, eu estou livre.
+        </p>
+      </div>
+    </section>
+    <section class="flex justify-center py-9">
+      <img
+        src="../assets/images/rubedo-footer.png"
+        alt="Círculo Alquímico"
+        class="w-auto h-auto hover:scale-105 transition-transform duration-500 ease-in-out"
+      />
+    </section>
+  </main>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
